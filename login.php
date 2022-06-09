0217c6955f547101d187f2bf0782bb52a8237d9c
Boyan Kloosterman <boyankloosterman1@gmail.com>
2022-06-09 11:48:16 +0200

    login toegevoegd

--- login.php
+++ login.php
@@ -3,63 +3,18 @@
 <!doctype html>
 <html lang="nl">
 
-<head>
-    <title>StoringApp / Meldingen</title>
-    <?php require_once '../head.php'; ?>
-</head>
+<form action="backend/loginController.php" method="POST">
 
-<body>
+<div class="form-group">
+    <label for="username">Gebruikersnaam: </label>
+    <input type="text" name="username" id="username">
+</div>
 
-    <?php require_once '../header.php'; ?>
-    
-    <div class="container">
-        <?php session_start(); ?>
-        <a href="create.php">Nieuwe melding &gt;</a>
+<div class="form-group">
+    <label for="password">Wachtwoord: </label>
+    <input type="password" name="password" id="password">
+</div>
 
-        <?php if(isset($_GET['msg']))
-        {
-            echo "<div class='msg'>" . $_GET['msg'] . "</div>";
-        } ?>
+<input type="submit" value="Inloggen">
 
-       <?php
-       require_once "../backend/conn.php";
-       $query = "SELECT * FROM meldingen";
-       $statement = $conn->prepare($query);
-       $statement->execute();
-       $meldingen = $statement->fetchAll(PDO::FETCH_ASSOC);
-       ?>
-       
-    <table>
-        <tr>
-            <th>Titel</th>
-            <th>Inhoud</th>
-            <th>Melder</th>
-            <th>Overig</th>
-            <th>Prioriteit</th>
-            <th>Capaciteit</th>
-            <th>Gemeld op</th>
-            <th>Aanpassen</th>
-        </tr>
-       <?php foreach ($meldingen as $melding): ?>
-       <td> <?php echo $melding['attractie']; ?> </td>
-       <td> <?php echo $melding['type']; ?> </td>
-       <td><?php echo $melding['melder']; ?></td>
-       <td><?php echo$melding['overige_info']; ?></td>
-       <td><?php if($melding['prioriteit'] == 1){
-        echo "Ja";
-        }
-        else{ 
-        echo "Nee";
-        } ?> </td>
-        <td><?php echo$melding['capaciteit']; ?></td>
-        <td><?php echo$melding['gemeld_op']; ?></td>
-        <td> <a href="edit.php">Aanpassen</a></td>
-        </tr>
-        <?php endforeach;?>
-    </table>
-        
-    </div>  
-
-</body>
-
-</html>
+</form>
--- meldingen/edit.php
+++ meldingen/edit.php
@@ -28,28 +28,28 @@
 
             <div class="form-group">
                 <label for="attractie">Naam attractie:</label>
-                <input type= "text" name ="titel" value="<?php echo $melding['attractie']; ?>" id = "attractie">
+                <input type= "text" name ="titel" value="<?php echo $meldingen['attractie']; ?>" id = "attractie">
             </div>
             <!-- Zorg dat het type wordt getoond, net als de naam hierboven -->
             <div class="form-group">
                 <label for="capaciteit">Capaciteit p/uur:</label>
                 <input type="number" min="0" name="capaciteit" id="capaciteit" class="form-input"
-                    value="<?php echo $melding['capaciteit']; ?>">
+                    value="<?php echo $meldingen['capaciteit']; ?>">
             </div>
             <div class="form-group">
                 <label for="prioriteit">Prio:</label>
                 <!-- Let op: de checkbox blijft nu altijd uit, pas dit nog aan -->
-                <input type="checkbox" name="prioriteit" id="prioriteit" value="<?php echo $melding['prioriteit']; ?>">
+                <input type="checkbox" name="prioriteit" id="prioriteit" value="<?php echo $meldingen['prioriteit']; ?>">
                 <label for="prioriteit">Melding met prioriteit</label>
             </div>
             <div class="form-group"> 
                 <label for="melder">Naam melder:</label>
                 <!-- Voeg hieronder nog een value-attribuut toe, zoals bij capaciteit -->
-                <input type="text" name="melder" id="melder" class="form-input" value="<?php echo $melding['melder']; ?>">
+                <input type="text" name="melder" id="melder" class="form-input" value="<?php echo $meldingen['melder']; ?>">
             </div>
             <div class="form-group">
                 <label for="overig">Overige info:</label>
-                <textarea name="overig" id="overig" class="form-input" rows="4"><?php echo $melding['overig']; ?> </textarea>
+                <textarea name="overig" id="overig" class="form-input" rows="4"><?php echo $meldingen['overig']; ?> </textarea>
             </div>
             
             <input type="submit" value="Melding opslaan">
